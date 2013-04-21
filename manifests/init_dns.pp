9fd089817d22d2a3a909743e134627af5bbc6e53
Sascha  Rüssel <s.ruessel@web.de>
2013-04-21 22:24:00 +0200

    created multi-vm setup for seperate, by foreman managed, dns-server

---
+++ manifests/init_dns.pp
@@ -0,0 +1,34 @@
+if ! $::osfamily {
+  case $::operatingsystem {
+    'RedHat', 'Fedora', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'CloudLinux', 'PSBM', 'OracleLinux', 'OVS', 'OEL': {
+      $osfamily = 'RedHat'
+    }
+    'ubuntu', 'debian': {
+      $osfamily = 'Debian'
+    }
+    'SLES', 'SLED', 'OpenSuSE', 'SuSE': {
+      $osfamily = 'Suse'
+    }
+    'Solaris', 'Nexenta': {
+      $osfamily = 'Solaris'
+    }
+    default: {
+      $osfamily = $::operatingsystem
+    }
+  }
+}
+
+if ! $::lsbdistcodename {
+  case $::operatingsystem {
+    'Debian' : {
+      case $::operatingsystemrelease {
+        '6.0.6' : {
+	  $lsbdistcodename = 'squeeze'
+	}
+      }
+    } 
+  }
+}
+
+include foreman_proxy
+include dns
